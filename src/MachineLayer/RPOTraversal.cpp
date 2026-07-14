0f715283fc5067e623eb2bcc990993ddeb77f332
UjeNeTORT <tikhonovty@gmail.com>
2026-07-15 00:42:57 +0300

    implemnent RPO traversal

--- src/MachineLayer/RPOTraversal.cpp
+++ src/MachineLayer/RPOTraversal.cpp
@@ -1,1 +1,45 @@
 #include "RPOTraversal.h"
+#include "MachineBB.h"
+
+#include "MachineFunction.h"
+
+#include <list>
+#include <unordered_set>
+
+using namespace Balance;
+
+void RPOTraversal::dfs(MachineBB *MBB, std::unordered_set<MachineBB *> &Visited) {
+    Visited.insert(MBB);
+
+    for (MachineBB *Succ : MBB->getSuccessors()) {
+        if (Visited.find(Succ) == Visited.end()) {
+            dfs(Succ, Visited);
+        }
+    }
+    PO.push_back(MBB);
+
+    RPO.insert(RPO.end(), PO.rbegin(), PO.rend());
+}
+
+RPOTraversal::RPOTraversal(MachineFunction &MF) {
+    compute(MF);
+}
+
+void RPOTraversal::compute(MachineFunction &MF) {
+    RPO.clear();
+    PO.clear();
+    std::unordered_set<MachineBB *> Visited;
+
+    if (MF.begin() != MF.end()) {
+        dfs(&*MF.begin(), Visited);
+    }
+}
+
+std::list<MachineBB *> RPOTraversal::getRPO() && { return std::move(RPO); }
+std::list<MachineBB *> RPOTraversal::getPO() && { return std::move(PO); }
+
+RPOTraversal::iterator RPOTraversal::begin() { return RPO.begin(); }
+RPOTraversal::iterator RPOTraversal::end()   { return RPO.end(); }
+
+RPOTraversal::const_iterator RPOTraversal::begin() const { return RPO.begin(); }
+RPOTraversal::const_iterator RPOTraversal::end()   const { return RPO.end(); }
--- src/MachineLayer/RPOTraversal.h
+++ src/MachineLayer/RPOTraversal.h
@@ -2,7 +2,34 @@
 #include "MachineBB.h"
 
 #include <list>
+#include <vector>
+#include <unordered_set>
 
 namespace Balance {
-std::list<MachineBB *> getRPO(MachineFunction::iterator I);
+
+class MachineBB;
+
+class RPOTraversal {
+    std::list<MachineBB *> RPO;
+    std::list<MachineBB *> PO;
+
+    void dfs(MachineBB *MBB, std::unordered_set<MachineBB *> &Visited);
+
+public:
+    using iterator = std::list<MachineBB *>::iterator;
+    using const_iterator = std::list<MachineBB *>::const_iterator;
+
+    explicit RPOTraversal(MachineFunction &MF);
+
+    void compute(MachineFunction &MF);
+    iterator begin();
+    iterator end();
+
+    std::list<MachineBB *> getRPO() &&;
+    std::list<MachineBB *> getPO() &&;
+
+    const_iterator begin() const;
+    const_iterator end()   const;
+};
+
 } // namespace Balance
